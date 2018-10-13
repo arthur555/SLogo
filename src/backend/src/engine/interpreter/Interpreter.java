6f86b1683cadcb6b898b1d9abf57957d0e68134a
Haotian Wang <haotian.wang@duke.edu>
2018-10-13 12:33:38 -0400

    Modify Prof. Duvall's program parser

--- src/backend/src/engine/interpreter/Interpreter.java
+++ src/backend/src/engine/interpreter/Interpreter.java
@@ -1,9 +1,9 @@
-package engine;
+package engine.interpreter;
 
 /**
  * Take in abstract syntax tree from Parser.
  * Generate a list of TurtleAction from the abstract syntax tree.
  * Or alternatively, instead of creating a TurtleAction object, output the desired coordinates and direction for the turtle after the end of each tick cycle.
  */
-public class Interpreter {
+public interface Interpreter {
 }
