d6d850dbb4d379e573844a8617c76e4d81a03f15
Shenghong Zhao <sz165@duke.edu>
2018-10-18 10:09:29 -0400

    figured out how to create an observable out of model

--- src/frontend/src/fake_model/TurtleModel.java
+++ src/frontend/src/fake_model/TurtleModel.java
@@ -1,29 +1,51 @@
 package fake_model;
 
-public class TurtleModel {
-    private int x;
-    private int y;
-    private double angle;
-    private boolean visible;
-    private boolean penDown;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.property.*;
+import javafx.beans.value.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.Observer;
+
+public class TurtleModel{
+    private Integer x;
+    private Integer y;
+    private SimpleDoubleProperty angle;
+    private SimpleBooleanProperty visible;
+    private SimpleBooleanProperty penDown;
+    private ObservableList<Integer> points;
+
 
     public TurtleModel(){
         x = 0;
         y = 0;
-        angle = 0;
-        visible = true;
-        penDown = true;
+        points = FXCollections.observableArrayList();
+        points.add(x);
+        points.add(y);
+        angle = new SimpleDoubleProperty(0);
+        visible = new SimpleBooleanProperty(true);
+        penDown = new SimpleBooleanProperty(true);
+
     }
 
-    boolean isPenDown(){return penDown;}
-    boolean isVisible(){return visible;}
-    void setPenDown(boolean down){penDown=down;}
-    void setVisible(boolean visible){this.visible=visible;}
 
-    int getX(){return x;}
-    int getY(){return y;}
-    double getAnlge(){return angle;}
-    void setX(int x){this.x = x;}
-    void setY(int y){this.y = y;}
-    void setAngle(int angle){this.angle = angle;}
+    public SimpleBooleanProperty isPenDown(){return penDown;}
+    public SimpleBooleanProperty isVisible(){return visible;}
+    public void setPenDown(boolean down){penDown.setValue(down);}
+    public void setVisible(boolean visible){this.visible.setValue(visible);}
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+    public SimpleDoubleProperty getAnlge(){return angle;}
+
+    public void setX(int x){this.x=x;
+    points.set(0,x);
+    points.set(1,y);}
+    public void setY(int y){this.y=y;
+    points.set(1,y);}
+    public void setAngle(int angle){this.angle.setValue(angle);}
+
+    public ObservableList<Integer> getPoints (){ return points;}
 }
--- src/frontend/src/view/TurtleView.java
+++ src/frontend/src/view/TurtleView.java
@@ -1,5 +1,10 @@
 package view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableObjectValue;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -7,23 +12,45 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import model.TurtleModel;
+import fake_model.TurtleModel;
 import view.utils.BackgroundUtils;
+
+import java.util.EventListener;
 
 public class TurtleView {
     public static final String turtleImage = "turtle_image_button.png";
     static int TURTLE_VIEW_WIDTH = MainView.SCREEN_HEIGHT;
 
+    private int x;
+    private int y;
+    private boolean penDown;
     private GridPane root;
     private ImageView turtle;
     private Color penColor;
 
-    TurtleView(TurtleModel turtleModel) {
+    public TurtleView(TurtleModel turtleModel) {
+        turtle = new ImageView();
         root = new GridPane();
         root.getStyleClass().add("canvas");
         root.setPrefWidth(200);
         root.setPrefHeight(200);
-        //root.getChildren().add(new Rectangle(200, 200, 200, 200));
+
+
+        turtle.rotateProperty().bind(turtleModel.getAnlge());
+        turtle.visibleProperty().bind(turtleModel.isVisible());
+
+        turtleModel.getPoints().addListener(new ListChangeListener<Integer>() {
+            @Override
+            public void onChanged(Change<? extends Integer> c) {
+                System.out.println(c.getList().get(0));
+                System.out.println(c.getList().get(1));
+            }
+        });
+
+
+
+        //
+
     }
 
     public Pane view() { return root; }
@@ -37,4 +64,11 @@ public class TurtleView {
     }
     public void setPenColor(Color c) {penColor = c;}
     public void toWards(Point2D pos) { }
+    public void forWards(int distance){ }
+
+    String command;
+
+
+
+
 }
