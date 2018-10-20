a0e2f151a92b88c72b4b30eb32c1212b89b7dd3d
Haotian Wang <haotian.wang@duke.edu>
2018-10-20 02:23:29 -0400

    Change statemachine to Interpreter instead of parser

--- src/backend/src/engine/interpreter/CrudeInterpreter.java
+++ src/backend/src/engine/interpreter/CrudeInterpreter.java
@@ -1,4 +1,45 @@
 package engine.interpreter;
 
+import engine.commands.TurtleCommand;
+import engine.parser.SLogoAST;
+import model.TurtleModel;
+
+import java.util.List;
+
+/**
+ * This is a crude interpreter implementation.
+ *
+ * @author Haotian Wang
+ */
 public class CrudeInterpreter implements Interpreter{
+
+    private StateMachine myVariables;
+    
+    /**
+     * Read in the commands
+     *
+     * @param ast : SLogoAST representing the command tree.
+     */
+    @Override
+    public void readCommands(SLogoAST ast) {
+
+    }
+
+    /**
+     * This returns a list of actionable TurtleCommand after the interpreter interprets the SLogoAST.
+     *
+     * @return A list of actionable TurtleCommand.
+     */
+    @Override
+    public List<TurtleCommand<TurtleModel>> outputTurtleCommands() {
+        return null;
+    }
+
+    /**
+     * This clears the internal AST stored in the interpreter.
+     */
+    @Override
+    public void clearCommands() {
+
+    }
 }
