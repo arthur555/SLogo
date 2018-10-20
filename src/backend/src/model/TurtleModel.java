5f31e7fc85c97bc496c1905637177697590236e6
Haotian Wang <haotian.wang@duke.edu>
2018-10-20 02:13:23 -0400

    Add two default methods to TurtleModel such that it reads in single TurtleCommand and a list of TurtleCommands

--- src/backend/src/model/TurtleModel.java
+++ src/backend/src/model/TurtleModel.java
@@ -2,6 +2,30 @@ package model;
 
 import engine.commands.TurtleCommand;
 
+import java.util.List;
+
+/**
+ * This interface handles the behavior of the underlying turtle model.
+ *
+ * @author Haotian Wang
+ */
 public interface TurtleModel {
-    void act(TurtleCommand<TurtleModel> command);
+
+    /**
+     * This is a default method to all classes extending TurtleModel such that all the turtle models take in a list of TurtleCommand and act on them.
+     *
+     * @param commands: A list of TurtleCommand.
+     */
+    default void actOnListOfCommands(List<TurtleCommand<TurtleModel>> commands) {
+        commands.forEach(a -> a.update(this));
+    }
+
+    /**
+     * This is a default method to all classes extending TurtleModel such that all turtle models take in a TurtleCommand and acts on it.
+     *
+     * @param command: A single TurtleCommand
+     */
+    default void actOnCommand(TurtleCommand<TurtleModel> command) {
+        command.update(this);
+    }
 }
