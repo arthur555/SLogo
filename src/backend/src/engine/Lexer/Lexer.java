bc50a530eae8fb91141b4f597e1009e6e10ae19d
Haotian Wang <haotian.wang@duke.edu>
2018-10-20 22:59:44 -0400

    Redo AST

---
+++ src/backend/src/engine/Lexer/Lexer.java
@@ -0,0 +1,25 @@
+package engine.Lexer;
+
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * This interface handles the tokenization of input strings into a set of tokens.
+ *
+ * @author Haotian Wang
+ */
+public interface Lexer {
+    /**
+     * This processes the user input raw String.
+     *
+     * @param input: A user input raw String.
+     */
+    void readString(String input);
+
+    /**
+     * Return a list of Token from the input String, after translation by two translators.
+     *
+     * @return A list of Token from the input String.
+     */
+    Queue<Token> getTokens();
+}
