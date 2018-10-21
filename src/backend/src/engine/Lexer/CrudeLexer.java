43564a3df8997c67bf72e4c1564e38e4a24e4cd8
Haotian Wang <haotian.wang@duke.edu>
2018-10-21 01:30:38 -0400

    Add unmodifiable grammar map to the lexer so that tokens are generated correctly

--- src/backend/src/engine/Lexer/CrudeLexer.java
+++ src/backend/src/engine/Lexer/CrudeLexer.java
@@ -1,17 +1,81 @@
 package engine.Lexer;
 
+import engine.commands.Forward;
 import engine.errors.CommandSyntaxException;
 import engine.parser.CrudeParser;
 import engine.translator.LanguageTranslator;
 import engine.translator.TypeTranslator;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
+/**
+ * This CrudeLexer reads in a String input and output a queue of Tokens representing the basic syntactic building blocks in the SLogo language.
+ *
+ * @author Haotian Wang
+ */
 public class CrudeLexer implements Lexer{
     private static final String PREFIX = "engine/translator/languages/";
     private static final String DEFAULT_LANGUAGE = "English";
     private static final String SYNTAX = "Syntax";
+    private static final Map<String, String> myGrammerMap;
+    static {
+        Map<String, String> grammarMap = new HashMap<>();
+        grammarMap.put("Forward", "unary");
+        grammarMap.put("Backward", "unary");
+        grammarMap.put("Left", "unary");
+        grammarMap.put("Right", "unary");
+        grammarMap.put("SetHeading", "unary");
+        grammarMap.put("SetTowards", "unary");
+        grammarMap.put("SetPosition", "unary");
+        grammarMap.put("Minus", "unary");
+        grammarMap.put("Random", "unary");
+        grammarMap.put("Sine", "unary");
+        grammarMap.put("Cosine", "unary");
+        grammarMap.put("Tangent", "unary");
+        grammarMap.put("ArcTangent", "unary");
+        grammarMap.put("NaturalLog", "unary");
+        grammarMap.put("Not", "unary");
+
+        grammarMap.put("PenUp", "direct");
+        grammarMap.put("PenDown", "direct");
+        grammarMap.put("ShowTurtle", "direct");
+        grammarMap.put("HideTurtle", "direct");
+        grammarMap.put("Home", "direct");
+        grammarMap.put("ClearScreen", "direct");
+        grammarMap.put("XCoordinate", "direct");
+        grammarMap.put("YCoordinate", "direct");
+        grammarMap.put("Heading", "direct");
+        grammarMap.put("IsPenDown", "direct");
+        grammarMap.put("IsShowing", "direct");
+        grammarMap.put("Pi", "direct");
+
+        grammarMap.put("Sum", "binary");
+        grammarMap.put("Difference", "binary");
+        grammarMap.put("Quotient", "binary");
+        grammarMap.put("Remainder", "binary");
+        grammarMap.put("Power", "binary");
+        grammarMap.put("LessThan", "binary");
+        grammarMap.put("GreaterThan", "binary");
+        grammarMap.put("Equal", "binary");
+        grammarMap.put("NotEqual", "binary");
+        grammarMap.put("And", "binary");
+        grammarMap.put("Or", "binary");
+
+        grammarMap.put("MakeVariable", "assign");
+
+        grammarMap.put("Repeat", "condition");
+        grammarMap.put("If", "condition");
+
+        grammarMap.put("DoTimes", "dotimes");
+
+        grammarMap.put("For", "for");
+
+        grammarMap.put("IfElse", "ifelse");
+
+        grammarMap.put("MakeUserInstruction", "create");
+
+        myGrammerMap = Collections.unmodifiableMap(grammarMap);
+    }
 
     private TypeTranslator myType;
     private LanguageTranslator myLanguage;
@@ -73,7 +137,7 @@ public class CrudeLexer implements Lexer{
                     chunk = myLanguage.getSymbol(chunk);
                     start = end + 1;
                     end++;
-                    type = cateogrize(chunk);
+                    type = grammarMap.get(type);
                 }
                 myTokens.offer(new Token(chunk, type));
             } catch (CommandSyntaxException e) {
@@ -83,16 +147,6 @@ public class CrudeLexer implements Lexer{
         if (start != input.length() || end != input.length()) {
             throw new CommandSyntaxException("The input String contains tokens that are not properly defined in the properties files.");
         }
-    }
-
-    /**
-     * This method categorize the input command into its grammar types. Details on grammar types are found in the EBNF reference file.
-     *
-     * @param chunk: An input String type such as Forward or Left.
-     * @return A String indicating the type of the command such as Unary and Binary.
-     */
-    private String cateogrize(String chunk) {
-
     }
 
     /**
