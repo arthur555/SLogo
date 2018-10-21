32f8b39e83c455e330e08788d351f00bbb374b1a
Haotian Wang <haotian.wang@duke.edu>
2018-10-21 12:12:26 -0400

    Added MakeUserInstruction grammar

--- src/backend/src/engine/slogoast/IfElse.java
+++ src/backend/src/engine/slogoast/IfElse.java
@@ -12,16 +12,20 @@ import java.util.List;
 public class IfElse extends Expression {
     private Token condition;
     private Expression expr;
-    private Token start;
-    private Token end;
+    private Token startA;
     private List<Expression> exprListA;
+    private Token endA;
+    private Token startB;
     private List<Expression> exprListB;
+    private Token endB;
 
-    public IfElse(Token a, Expression val, Token listStart, Token listEnd, List<Expression> listA, List<Expression> listB) {
+    public IfElse(Token a, Expression val, Token listStart, List<Expression> listA,  Token listEnd, Token list2Start, List<Expression> listB, Token list2End) {
         condition = a;
         expr = val;
-        start = listStart;
-        end = listEnd;
+        startA = listStart;
+        endA = listEnd;
+        startB = list2Start;
+        endB = list2End;
         exprListA = listA;
         exprListB = listB;
     }
