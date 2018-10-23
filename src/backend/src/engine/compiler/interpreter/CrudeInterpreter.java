3f93859bb2958c0b976663418a343dd059f56657
Haotian Wang <haotian.wang@duke.edu>
2018-10-23 09:43:20 -0400

    Finish interpreter interface finally

--- src/backend/src/engine/compiler/interpreter/CrudeInterpreter.java
+++ src/backend/src/engine/compiler/interpreter/CrudeInterpreter.java
@@ -14,33 +14,4 @@ import java.util.List;
  */
 public class CrudeInterpreter implements Interpreter{
 
-    private StateMachine myVariables;
-
-    /**
-     * Read in the commands
-     *
-     * @param ast : SLogoAST representing the command tree.
-     */
-    @Override
-    public void readCommands(Expression ast) {
-
-    }
-
-    /**
-     * This returns a list of actionable Command after the interpreter interprets the SLogoAST.
-     *
-     * @return A list of actionable Command.
-     */
-    @Override
-    public List<Command<TurtleModel>> outputTurtleCommands() {
-        return null;
-    }
-
-    /**
-     * This clears the internal AST stored in the interpreter.
-     */
-    @Override
-    public void clearCommands() {
-
-    }
 }
