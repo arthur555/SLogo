5201c55862973aeb095f71418ed79a716d9eea5d
Inchan Hwang <inchankorea@gmail.com>
2018-10-25 14:35:40 -0400

    implement loading previous commands via click

--- src/frontend/src/view/HistoryView.java
+++ src/frontend/src/view/HistoryView.java
@@ -1,20 +1,25 @@
 package view;
 
 import app.SLogoApp;
+import javafx.event.EventHandler;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import view.utils.PrettyUI;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class HistoryView {
     static final int HISTORY_VIEW_WIDTH =
             SLogoApp.APP_SCREEN_WIDTH - SidebarView.SIDEBAR_VIEW_WIDTH - CanvasView.TURTLE_VIEW_WIDTH;
     private final int FONT_SIZE = 12;
 
-
     private ScrollPane root;
     private VBox display;
+    private Consumer<String> onHistoryClick;
 
     HistoryView() {
         display = new VBox(10);
@@ -30,25 +35,42 @@ public class HistoryView {
         root.setContent(display);
     }
 
+    public void registerOnHistoryClick(Consumer<String> handler) { onHistoryClick = handler; }
+
     public ScrollPane view() { return root; }
 
     public void addText(String text, double retVal) {
-        var flo = new TextFlow();
-        PrettyUI.litBgTheme(flo);
-        flo.getChildren().addAll(PrettyUI.highlight(text.trim(), FONT_SIZE));
-        flo.getChildren().add(new Text("\n>> "));
-        flo.getChildren().addAll(PrettyUI.number(String.valueOf(retVal), " ", FONT_SIZE));
-        display.getChildren().add(flo);
+        display.getChildren().add(new HistoryUnit(text, retVal).flow());
     }
 
     public void displayError(String text, Exception e) {
-        var flo = new TextFlow();
-        PrettyUI.litBgTheme(flo);
-        flo.getChildren().addAll(PrettyUI.highlight(text.trim(), FONT_SIZE));
-        flo.getChildren().add(new Text("\n"));
-        flo.getChildren().addAll(PrettyUI.error(e.getMessage().trim(), FONT_SIZE));
-        display.getChildren().add(flo);
+        display.getChildren().add(new HistoryUnit(text, e).flow());
     }
 
     public void setLanguage(String lang) { }
+
+    private class HistoryUnit {
+        private TextFlow flow;
+
+        private HistoryUnit(String command) {
+            flow = new TextFlow();
+            PrettyUI.litBgTheme(flow);
+            flow.getChildren().addAll(PrettyUI.highlight(command.trim(), FONT_SIZE));
+        }
+
+        private HistoryUnit(String command, Exception e) {
+            this(command);
+            flow.getChildren().add(new Text("\n"));
+            flow.getChildren().addAll(PrettyUI.error(e.getMessage().trim(), FONT_SIZE));
+        }
+
+        private HistoryUnit(String command, double retVal) {
+            this(command);
+            flow.getChildren().add(new Text("\n>> "));
+            flow.getChildren().addAll(PrettyUI.number(String.valueOf(retVal), " ", FONT_SIZE));
+            flow.setOnMousePressed(ev -> onHistoryClick.accept(command));
+        }
+
+        private TextFlow flow() { return flow; }
+    }
 }
