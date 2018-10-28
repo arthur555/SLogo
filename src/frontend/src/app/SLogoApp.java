f9b4c97c6941b7627005be73a39f3a399b743fc7
Shenghong Zhao <sz165@duke.edu>
2018-10-28 12:09:45 -0400

    zomming in and out

--- src/frontend/src/app/SLogoApp.java
+++ src/frontend/src/app/SLogoApp.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.ModelModule;
 import view.ViewModule;
